ebf93f426c7391b8b97d613660a06de2a61b53ea
unknown <mahmodulshahin665@gmail.com>
2022-04-04 17:04:21 +0600

    add one more btn handler

--- dom.js
+++ dom.js
@@ -1,3 +1,4 @@
+//add border btn
 document.getElementById('add-border').addEventListener('click', function () {
     const fndContainer = document.getElementById('friend-container');
     fndContainer.style.border = '3px solid black';
@@ -5,6 +6,7 @@ document.getElementById('add-border').addEventListener('click', function () {
     fndContainer.style.padding = "0px 10px 0px 20px";
     fndContainer.style.borderRadius = "25px";
 });
+//background color btn
 function addBackgroundColor() {
     const friends = document.getElementsByClassName('friend');
     for (const friend of friends) {
@@ -14,3 +16,14 @@ function addBackgroundColor() {
         friend.style.color = 'white'
     }
 }
+//add more friend btn
+document.getElementById('add-friend').addEventListener('click', function () {
+    const fndContainer = document.getElementById('friend-container');
+    const friendDiv = document.createElement('div');
+    friendDiv.classList.add('friend');
+    friendDiv.innerHTML = `
+    <h3 class="friend-name">New friend</h3>
+    <p>Sint, tempore.</p>
+    `
+    fndContainer.appendChild(friendDiv);
+})
