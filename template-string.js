ff05c400003cf671dee91c7c233c40fbe70686f5
unknown <mahmodulshahin665@gmail.com>
2022-04-05 22:02:23 +0600

    exploring backtick quote

---
+++ template-string.js
@@ -0,0 +1,18 @@
+const priya = 'Asif Akbar';
+const meye = "Meye tumi ki dokko chino";
+//backtick quote 
+const kobita = `kobita tumi sopno charini`;
+
+//example of oldschool multiline system
+const multiline = 'this is my first line. \n' +
+    'this is my second line. \n' +
+    'this is my third line. \n' +
+    'this is my fourth line.\n';
+console.log('multiline -- ', multiline)
+
+//example of new multiline system on es6;
+const newMultiline = `This is my first new line. 
+This is my second new line.
+This is my thrid new line.
+This is my fourth new line.`;
+console.log('newMultiline -- ', newMultiline)
