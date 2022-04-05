e913a62860b7471d423464ace8a8a7ba61ef0a50
unknown <mahmodulshahin665@gmail.com>
2022-04-05 22:36:48 +0600

    add more backtick example

--- template-string.js
+++ template-string.js
@@ -8,11 +8,26 @@ const multiline = 'this is my first line. \n' +
     'this is my second line. \n' +
     'this is my third line. \n' +
     'this is my fourth line.\n';
-console.log('multiline -- ', multiline)
 
 //example of new multiline system on es6;
 const newMultiline = `This is my first new line. 
 This is my second new line.
 This is my thrid new line.
 This is my fourth new line.`;
-console.log('newMultiline -- ', newMultiline)
+
+//more example about backtick;
+//string er modde variable er name diye variable er man boshanoke string interpolation bole.
+//oldschool system;
+const fnCount = 5;
+const old = '<h3 class="friend-name">friend-5</h3>'
+const old2 = '<h3 class="friend-name">friend-' + fnCount + '</h3>';
+//new system
+const new1 = ` <h3 class="friend-name">friend-${fnCount}</h3>`;
+//=========================
+//more about string interpolation
+//template literals string er bitore iccemoto javaScript er code(expression) likte parbo.
+const first = 'Mamun';
+const last = 'Chowdhury';
+const fullNameOldSys = 'This person name is: ' + first + ' ' + last;
+const fullNameNewSys = `This is person name is: ${first} ${last}. He lives in Dhaka.`;
+
